228da0a0bee1385ac96f83a948fa788e6bcdc5b2
Álvaro Santos <agvdsbs@gmail.com>
2026-06-30 16:58:58 -0300

    feat: todos os grafos

---
+++ testes.pl
@@ -0,0 +1,14 @@
+% - grafo1 -
+
+vertice(a).
+vertice(b).
+vertice(c).
+vertice(d).
+vertice(e).
+
+adj(a,b). adj(b,a).
+adj(a,d). adj(d,a).
+adj(b,d). adj(d,b).
+adj(c,e). adj(e,c).
+adj(d,c). adj(c,d).
+
